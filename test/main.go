bb6dba8b65d46bce4a27f4787c1a5c4fdcb423e0
ityike <yuanfeng634@gmail.com>
2019-01-01 22:23:00 +0800

    add proto file

---
+++ test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"futugg"
+	_ "futugg/handlers"
+	// "futugg/pb/InitConnect"
+	// "github.com/golang/protobuf/proto"
+)
+
+func main() {
+
+	block := make(chan bool)
+	cli := futugg.New("0.0.0.0", "11250", "")
+
+	futugg.Cmd("send.GetGlobalState", cli)
+	cli.KeepAlive()
+
+	// recv
+	go func() {
+		fmt.Println("start recv data")
+		cli.Recv()
+	}()
+
+	fmt.Println("vvvvvv")
+	<-block
+}
