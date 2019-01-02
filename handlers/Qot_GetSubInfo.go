363b101f82f96c6cc5329ce769e68a91696088cc
ityike <yuanfeng634@gmail.com>
2019-01-02 23:25:26 +0800

    Add Qot_GetSubInfo

---
+++ handlers/Qot_GetSubInfo.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"fmt"
+	"futugg"
+	"futugg/pb/Qot_GetSubInfo"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func init() {
+	futugg.SetHandlerId(uint32(3003), "Qot_GetSubInfo")
+	var err error
+	err = futugg.On("send.Qot_GetSubInfo", QotGetSubInfoSend)
+	err = futugg.On("recv.Qot_GetSubInfo", QotGetSubInfoRecv)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func QotGetSubInfoSend(conn *futugg.FutuGG, isReqAllConn bool) error {
+	pack := &futugg.FutuPack{}
+	pack.SetProto(uint32(3003))
+
+	reqData := &Qot_GetSubInfo.Request{
+		C2S: &Qot_GetSubInfo.C2S{
+			IsReqAllConn: &isReqAllConn,
+		},
+	}
+	pbData, err := proto.Marshal(reqData)
+	if err != nil {
+		return fmt.Errorf("marshal error: %s", err)
+	}
+
+	pack.SetBody(pbData)
+	err = conn.Send(pack)
+
+	return err
+}
+
+func QotGetSubInfoRecv(data []byte) error {
+	resp := &Qot_GetSubInfo.Response{}
+	err := proto.Unmarshal(data, resp)
+	if err != nil {
+		return fmt.Errorf("marshal error: %s", err)
+	}
+
+	fmt.Println(resp)
+
+	return nil
+}
