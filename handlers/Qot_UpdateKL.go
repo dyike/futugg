c9f1482b72c4a2fdfe4cb36d360b9d4210042ab2
yuanfeng <yuanfeng@bilibili.com>
2019-01-04 19:09:21 +0800

    Add Qot_UpdateKL

---
+++ handlers/Qot_UpdateKL.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+    "fmt"
+    "futugg"
+    "futugg/pb/Qot_UpdateKL"
+
+    "github.com/golang/protobuf/proto"
+)
+
+func init() {
+    futugg.SetHandlerId(uint32(3007), "Qot_UpdateKL")
+
+    var err error
+    err = futugg.On("recv.Qot_UpdateKL", QotUpdateBasicQotRecv)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func QotUpdateKLRecv(data []byte) error {
+    resp := &Qot_UpdateKL.Response{}
+    err := proto.Unmarshal(data, resp)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    return nil
+}
