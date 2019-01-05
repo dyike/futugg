ac42b58e573c0ef23c4f7498e3c81fd76aa80fce
ityike <yuanfeng634@gmail.com>
2019-01-05 23:01:19 +0800

    Add Qot_GetPlateSecurity

---
+++ handlers/Qot_GetPlateSecurity.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+    "fmt"
+    "futugg"
+    "futugg/pb/Qot_GetPlateSecurity"
+
+    "github.com/golang/protobuf/proto"
+)
+
+func init() {
+    futugg.SetHandlerId(uint32(3205), "Qot_GetPlateSecurity")
+    var err error
+    err = futugg.On("send.Qot_GetPlateSecurity", QotGetPlateSecuritySend)
+    err = futugg.On("recv.Qot_GetPlateSecurity", QotGetPlateSecurityRecv)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func QotGetPlateSecuritySend(conn *futugg.FutuGG, stockCode string) error {
+    pack := &futugg.FutuPack{}
+    pack.SetProto(uint32(3205))
+
+    security := transStockCode(stockCode)
+
+    reqData := &Qot_GetPlateSecurity.Request{
+        C2S: &Qot_GetPlateSecurity.C2S{
+            Plate: security,
+        },
+    }
+    pbData, err := proto.Marshal(reqData)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    pack.SetBody(pbData)
+    err = conn.Send(pack)
+
+    return err
+}
+
+func QotGetPlateSecurityRecv(data []byte) error {
+    resp := &Qot_GetPlateSecurity.Response{}
+    err := proto.Unmarshal(data, resp)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    fmt.Println(resp)
+
+    return nil
+}
