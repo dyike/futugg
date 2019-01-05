5084b81aabbccf3477b7559ec62cae3a6639fd04
ityike <yuanfeng634@gmail.com>
2019-01-05 22:16:15 +0800

    Add Qot_UpdateBroker

---
+++ handlers/Qot_UpdateBroker.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+    "fmt"
+    "futugg"
+    "futugg/pb/Qot_UpdateBroker"
+
+    "github.com/golang/protobuf/proto"
+)
+
+func init() {
+    futugg.SetHandlerId(uint32(3009), "Qot_UpdateBroker")
+
+    var err error
+    err = futugg.On("recv.Qot_UpdateBroker", QotUpdateBrokerRecv)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func QotUpdateBrokerRecv(data []byte) error {
+    resp := &Qot_UpdateBroker.Response{}
+    err := proto.Unmarshal(data, resp)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    return nil
+}
