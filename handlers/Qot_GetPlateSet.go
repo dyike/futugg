0a40c633a7dd7d01253e9148747a3769dbea60b4
ityike <yuanfeng634@gmail.com>
2019-01-05 22:58:21 +0800

    Add Qot_GetPlateSet

---
+++ handlers/Qot_GetPlateSet.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+    "fmt"
+    "futugg"
+    "futugg/pb/Qot_GetPlateSet"
+
+    "github.com/golang/protobuf/proto"
+)
+
+func init() {
+    futugg.SetHandlerId(uint32(3204), "Qot_GetPlateSet")
+    var err error
+    err = futugg.On("send.Qot_GetPlateSet", QotGetPlateSetSend)
+    err = futugg.On("recv.Qot_GetPlateSet", QotGetPlateSetRecv)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func QotGetPlateSetSend(conn *futugg.FutuGG, marketNum int32, plateSetType int32) error {
+    pack := &futugg.FutuPack{}
+    pack.SetProto(uint32(3204))
+
+    reqData := &Qot_GetPlateSet.Request{
+        C2S: &Qot_GetPlateSet.C2S{
+            Market: &marketNum,
+            PlateSetType: &plateSetType,
+        },
+    }
+    pbData, err := proto.Marshal(reqData)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    pack.SetBody(pbData)
+    err = conn.Send(pack)
+
+    return err
+}
+
+func QotGetPlateSetRecv(data []byte) error {
+    resp := &Qot_GetPlateSet.Response{}
+    err := proto.Unmarshal(data, resp)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    fmt.Println(resp)
+
+    return nil
+}
