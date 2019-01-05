15b68d7849e105ec37931e5e18c0112c1162ab61
ityike <yuanfeng634@gmail.com>
2019-01-05 23:26:53 +0800

    Add Qot_GetHoldingChangeList

---
+++ handlers/Qot_GetHoldingChangeList.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+    "fmt"
+    "futugg"
+    "futugg/pb/Qot_GetHoldingChangeList"
+
+    "github.com/golang/protobuf/proto"
+)
+
+func init() {
+    futugg.SetHandlerId(uint32(3208), "Qot_GetHoldingChangeList")
+    var err error
+    err = futugg.On("send.Qot_GetHoldingChangeList", QotGetHoldingChangeListSend)
+    err = futugg.On("recv.Qot_GetHoldingChangeList", QotGetHoldingChangeListRecv)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func QotGetHoldingChangeListSend(conn *futugg.FutuGG, stockCode string, holderCategory  int32, beginTime string, endTime string) error {
+    pack := &futugg.FutuPack{}
+    pack.SetProto(uint32(3208))
+
+    security := transStockCode(stockCode)
+    reqData := &Qot_GetHoldingChangeList.Request{
+        C2S: &Qot_GetHoldingChangeList.C2S{
+            Security:         security,
+            HolderCategory:   &holderCategory,
+            BeginTime:        &beginTime,
+            EndTime:          &endTime,
+        },
+    }
+    pbData, err := proto.Marshal(reqData)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    pack.SetBody(pbData)
+    err = conn.Send(pack)
+
+    return err
+}
+
+func QotGetHoldingChangeListRecv(data []byte) error {
+    resp := &Qot_GetHoldingChangeList.Response{}
+    err := proto.Unmarshal(data, resp)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    fmt.Println(resp)
+
+    return nil
+}
