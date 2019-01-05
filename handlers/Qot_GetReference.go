7f9ae9893dbb63e624fa68cd51bfe7aa7200d085
ityike <yuanfeng634@gmail.com>
2019-01-05 23:07:28 +0800

    Add Qot_GetReference

---
+++ handlers/Qot_GetReference.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+    "fmt"
+    "futugg"
+    "futugg/pb/Qot_GetReference"
+
+    "github.com/golang/protobuf/proto"
+)
+
+func init() {
+    futugg.SetHandlerId(uint32(3206), "Qot_GetReference")
+    var err error
+    err = futugg.On("send.Qot_GetReference", QotGetReferenceSend)
+    err = futugg.On("recv.Qot_GetReference", QotGetReferenceRecv)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func QotGetReferenceSend(conn *futugg.FutuGG, stockCode string, referenceType int32) error {
+    pack := &futugg.FutuPack{}
+    pack.SetProto(uint32(3206))
+
+    security := transStockCode(stockCode)
+
+    reqData := &Qot_GetReference.Request{
+        C2S: &Qot_GetReference.C2S{
+            Security: security,
+            ReferenceType: &referenceType,
+        },
+    }
+    pbData, err := proto.Marshal(reqData)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    pack.SetBody(pbData)
+    err = conn.Send(pack)
+
+    return err
+}
+
+func QotGetReferenceRecv(data []byte) error {
+    resp := &Qot_GetReference.Response{}
+    err := proto.Unmarshal(data, resp)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    fmt.Println(resp)
+
+    return nil
+}
