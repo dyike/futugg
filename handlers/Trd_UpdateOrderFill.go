5b080b5a3f79c0383c6da801f2c8aad5c82aa97c
ityike <yuanfeng634@gmail.com>
2019-01-06 01:00:38 +0800

    Add Trd_UpdateOrderFill

---
+++ handlers/Trd_UpdateOrderFill.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+    "fmt"
+    "futugg"
+    "futugg/pb/Trd_UpdateOrderFill"
+
+    "github.com/golang/protobuf/proto"
+)
+
+func init() {
+    futugg.SetHandlerId(uint32(2218), "Trd_UpdateOrderFill")
+    var err error
+    err = futugg.On("recv.Trd_UpdateOrderFill", TrdUpdateOrderFillRecv)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+
+func TrdUpdateOrderFillRecv(data []byte) error {
+    resp := &Trd_UpdateOrderFill.Response{}
+    err := proto.Unmarshal(data, resp)
+    if err != nil {
+        return fmt.Errorf("marshal error: %s", err)
+    }
+
+    fmt.Println(resp)
+
+    return nil
+}
